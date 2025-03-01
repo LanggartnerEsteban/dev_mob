90f054b94756df003688d9be43e41fb5ed5e4c2e
LanggartnerEsteban <102729577+LanggartnerEsteban@users.noreply.github.com>
2025-03-01 11:52:57 +0100

    Ajout animation de favoris

--- src/apps/FavoritesPhonesApp.tsx
+++ src/apps/FavoritesPhonesApp.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { IRootState } from "../services/Store";
 import { Phone } from "../models/Phone";
 import PhoneCard from "../components/PhoneCard";
+import LottieView from "lottie-react-native";
 
 /**
  * Composant d'affichage de la page des favoris.
@@ -16,6 +17,7 @@ export function FavoritesPhonesApp() {
 
 	return (
 		<View>
+			<LottieView source={require("../../assets/favoritesAnimate.json")} autoPlay loop style={styles.animation}/>
 			{favorites?.length > 0 ? (
 				favorites?.map((phone) => (
 					<PhoneCard key={phone.id} phone={phone} />
@@ -26,3 +28,13 @@ export function FavoritesPhonesApp() {
 		</View>
 	);
 }
+
+// Définition du style de la page.
+const styles = StyleSheet.create({
+	animation: {
+		width: 100,
+		height: 100,
+		resizeMode: "stretch",
+		alignSelf: "center"
+	}
+});
